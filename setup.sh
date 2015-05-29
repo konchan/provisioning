160c74b05fa788b07dfda8f50c9b4e02e07586ce
KONNO Katsuyuki <konno.katsuyuki@nifty.com>
2015-05-30 08:10:58 +0900

    add opencv include some packages

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ brew update
 brew install python
 brew install ansible
 HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook -i hosts -vv localhost.yml
+brew install ffmpeg --with-libvorbis --with-libvpx
+pip install numpy
+brew install opencv --with-eigen --with-jasper --with-libtiff --with-qt --with-tbb
