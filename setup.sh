51256f2c5bed85c7279bc5671946c5b197b47ccf
konchan <konno.katsuyuki@nifty.com>
2015-06-12 20:39:30 +0900

    add scipy, scikit-learn

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ brew install python
 brew install ansible
 HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook -i hosts -vv localhost.yml
 brew install ffmpeg --with-libvorbis --with-libvpx
-pip install numpy
+pip install numpy scipy scikit-learn
 brew install opencv --with-eigen --with-jasper --with-libtiff --with-qt --with-tbb
